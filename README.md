90831d1cf69750debce45eada7cf43aa1d38c2ed
Redempt <instantiable@gmail.com>
2023-01-15 12:40:15 -0500

    Fix benchmarks

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # crunch_eval
 High-performance algebraic expression evaluator.
 
-Basic usage:
+## Basic usage:
 ```
 use crunch_eval::expr::Expr;
 let expr = Expr::compile("1 + 1").unwrap();
 let val: f64 = expr.evaluate_blank().unwrap();
 assert_eq!(val, 2.0);
 ```
+
+## Benchmarks:
--- benches/bench.rs
+++ benches/bench.rs
@@ -3,19 +3,19 @@ use crunch_eval::{env::ExprEnv, expr::Expr};
 use evalexpr::*;
 
 fn compile_expr(expr: &str) -> Expr<f32, 0> {
-    Expr::compile(expr, Default::default()).unwrap()
+    Expr::compile(expr).unwrap()
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("compile long expression crunch_eval", |b| b.iter(|| compile_expr(black_box("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4"))));
     c.bench_function("compile long expression evalexpr", |b| b.iter(|| evalexpr::eval("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4")));
     c.bench_function("evaluate long expression crunch_eval no flatten", |b| {
-        let expr = Expr::<f32, 0>::compile("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4", Default::default());
+        let expr = Expr::<f32, 0>::compile("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4");
         let expr = expr.unwrap();
         b.iter(|| expr.evaluate(&[]).unwrap());
     });
     c.bench_function("evaluate long expression crunch_eval flatten", |b| {
-        let expr = Expr::<f32, 0>::compile("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4", Default::default());
+        let expr = Expr::<f32, 0>::compile("6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4 + 6.5*7.8^2.3 + (3.5^3+7/2)^3 -(5*4/(2-3))*4");
         let expr = expr.unwrap().flatten().unwrap();
         b.iter(|| expr.evaluate(&[]).unwrap());
     });
@@ -28,7 +28,7 @@ fn criterion_benchmark(c: &mut Criterion) {
         "evaluate expression with function and variable crunch_eval",
         |b| {
             let env = ExprEnv::new(["x"]).with_func("double", |[x]: [f32; 1]| x * 2.0);
-            let expr = Expr::compile("double(x + 1)", env).unwrap();
+            let expr = Expr::compile_env("double(x + 1)", env).unwrap();
             let vars = &[25.0];
             b.iter(|| expr.evaluate(vars).unwrap());
         },
